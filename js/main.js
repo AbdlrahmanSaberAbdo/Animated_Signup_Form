a53a946ce1754e4aa4ad918ada987f6bcf6ade04
Abdlrahman Saber <abdlrahmansaber344@gmail.com>
2018-02-12 05:38:35 -0800

    Signup Form

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+$(document).ready(function(){
+
+	$(".input").on("focus", function() {
+		$(".login-form").addClass("focused");
+	})
+	$(".input-btn").on("click", function(e) {
+		e.preventDefault();
+		$(".login-form").removeClass("focused").addClass("loading");
+	})
+
+});
